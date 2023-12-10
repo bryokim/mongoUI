42d91230a5cfc9caf7738965a29939bd8f5005cc
Brian Kimathi <bryo.kim1@gmail.com>
2023-12-10 14:43:48 +0300

    feat: add navbar component

---
+++ app/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-toolbar id="navbar" :elevation="8">
+    <v-toolbar-title class="title">MongoUI</v-toolbar-title>
+    <v-spacer></v-spacer>
+
+    <div class="me-4">
+        MongoDB version X.X.X
+    </div>
+  </v-toolbar>
+</template>
+
+<style>
+#navbar {
+  background: #316a99;
+  height: 75px;
+}
+#navbar .title {
+  color: #91fd94;
+  font-family: Antic Slab, sans-serif;
+  font-size: 48px;
+  line-height: normal;
+  margin-left: 50px;
+}
+</style>
