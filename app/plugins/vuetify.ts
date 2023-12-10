7a42236dadd8bdf3acc7416ac9316f08441cf2ae
Brian Kimathi <bryo.kim1@gmail.com>
2023-12-10 14:36:42 +0300

    use custom theme in vuetify

--- app/plugins/vuetify.ts
+++ app/plugins/vuetify.ts
@@ -1,11 +1,34 @@
-import '@mdi/font/css/materialdesignicons.css'
+import "@mdi/font/css/materialdesignicons.css";
 
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
+import "vuetify/styles";
+import { createVuetify, type ThemeDefinition } from "vuetify";
+
+const myCustomTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: "#316A99",
+    surface: "#316a99",
+    primary: "#00FFFF",
+    "primary-2": "#91EAFD",
+    secondary: "#03DAC6",
+    error: "#c40000",
+    info: "#2196F3",
+    success: "#91FD94",
+    warning: "#FFD686",
+    danger: "#F5516E",
+    "purple-1": "#7000FF",
+    "purple-2": "#91A8FD"
+  },
+};
 
 export default defineNuxtPlugin((app) => {
   const vuetify = createVuetify({
-    // ... your configuration
-  })
-  app.vueApp.use(vuetify)
-})
+    theme: {
+      defaultTheme: "myCustomTheme",
+      themes: {
+        myCustomTheme,
+      },
+    }
+  });
+  app.vueApp.use(vuetify);
+});
