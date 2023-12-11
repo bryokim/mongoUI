2498c754ef12dea673a0041b63a97b0c3428f9ba
Brian Kimathi <bryo.kim1@gmail.com>
2023-12-11 18:59:49 +0300

    feat: add home page
    This is the page where the user is redirected after connecting.

---
+++ app/pages/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-layout class="mt-1">
+    <v-container>
+      <pre>{{ client }} </pre>
+    </v-container>
+  </v-layout>
+</template>
+
+<script>
+import { useClient } from "@/composables/useClient";
+
+export default {
+  data() {
+    return {
+      client: useClient().value,
+    };
+  },
+  methods: {
+    check() {
+      console.log(useClient().value);
+    },
+  },
+};
+</script>
