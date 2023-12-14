785f11ecfbcd2783db5c0038715b2f5f78e47531
Brian Kimathi <bryo.kim1@gmail.com>
2023-12-14 03:46:50 +0300

    refactor: change /home route to match all routes starting with /home
    The /home route can now match /home, /home/db and /home/db/collection.

---
+++ app/pages/home/[...slug].vue
@@ -0,0 +1,26 @@
+<template>
+  <v-layout class="mt-1">
+    <v-main style="height: calc(100vh - 80px)">
+      <v-container>
+        <pre v-if="$route.params.slug">
+          {
+            database: {{ $route.params.slug[0] }}
+            collection: {{ $route.params.slug[1] }}
+          }
+        </pre
+        >
+        <pre v-else> No collection selected </pre>
+      </v-container>
+    </v-main>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  setup() {
+    definePageMeta({
+      middleware: ["connected"],
+    });
+  },
+};
+</script>
