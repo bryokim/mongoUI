a52afd488af62b6624baadf967bb32d98c53012a
Brian Kimathi <bryo.kim1@gmail.com>
2023-12-14 15:02:39 +0300

    add breadcrumbs for database and collection navigation

--- app/pages/home/[...slug].vue
+++ app/pages/home/[...slug].vue
@@ -3,15 +3,35 @@
     <DatabaseNavigation></DatabaseNavigation>
 
     <v-main style="height: calc(100vh - 80px)">
+      <v-row class="mx-10">
+        <v-col cols="12">
+          <v-breadcrumbs class="pb-0">
+            <template v-slot:prepend>
+              <v-icon size="small" icon="mdi-database-outline"></v-icon>
+            </template>
+            <v-breadcrumbs-item :to="`/home/${database}`">
+              {{ database }}
+            </v-breadcrumbs-item>
+
+            <v-breadcrumbs-divider v-if="collection">
+              <v-icon icon="mdi-chevron-right"></v-icon>
+            </v-breadcrumbs-divider>
+
+            <v-breadcrumbs-item v-if="collection" color="primary">
+              {{ collection }}
+            </v-breadcrumbs-item>
+          </v-breadcrumbs>
+        </v-col>
+      </v-row>
+
       <v-container>
         <pre v-if="$route.params.slug">
           {
-            database: {{ $route.params.slug[0] }}
-            collection: {{ $route.params.slug[1] }}
+            database: {{ database }}
+            collection: {{ collection }}
           }
-        </pre
-        >
-        <pre v-else> No collection selected </pre>
+        </pre>
+        <pre v-else> No database selected </pre>
       </v-container>
     </v-main>
   </v-layout>
@@ -23,6 +43,12 @@ export default {
     definePageMeta({
       middleware: ["connected"],
     });
+  },
+  data() {
+    return {
+      database: this.$route.params.slug[0],
+      collection: this.$route.params.slug[1],
+    };
   },
 };
 </script>
