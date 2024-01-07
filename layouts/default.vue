88abbd3b671e108c868854c41ad35810a0581956
Brian Kimathi <bryo.kim1@gmail.com>
2024-01-07 23:04:03 +0300

    feat: add help button that navigates to documentation

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,5 +4,31 @@
     <v-main>
       <slot />
     </v-main>
+    <v-sheet location="bottom right" position="absolute" class="mb-7 me-4">
+      <v-btn
+        icon
+        variant="elevated"
+        rounded="rounded-circle"
+        color="secondary"
+        @click="openDocumentation"
+        style="z-index: 1000;"
+      >
+        <v-icon>mdi-help</v-icon>
+      </v-btn>
+    </v-sheet>
   </v-app>
 </template>
+
+<script>
+export default {
+  methods: {
+    openDocumentation() {
+      navigateTo("/documentation", {
+        open: {
+          target: "_blank",
+        },
+      });
+    },
+  },
+};
+</script>
